99c5924433b3ce289023385522b780ce736a509d
khandelwalshivam644-pixel <khandelwalshivam644@gmail.com>
2026-08-29 12:15:55 +0530

    function loc and iloc understand

--- pandaa.py
+++ pandaa.py
@@ -16,18 +16,60 @@
 # print(df.info())
 # print(df.describe())
 
-import pandas as pd
+# import pandas as pd
 
-data = {
-    "Name": ["Aman", "Riya", "Rahul", "Neha", "Vikas"],
-    "Age": [20, 21, 19, 22, 20],
-    "Marks": [78, 91, 65, 88, 72],
-    "City": ["Pune", "Mumbai", "Pune", "Delhi", "Mumbai"]
-}
+# data = {
+#     "Name": ["Aman", "Riya", "Rahul", "Neha", "Vikas"],
+#     "Age": [20, 21, 19, 22, 20],
+#     "Marks": [78, 91, 65, 88, 72],
+#     "City": ["Pune", "Mumbai", "Pune", "Delhi", "Mumbai"]
+# }
 
-df = pd.DataFrame(data)
-print(df.shape)
-print(df.Marks)
-print(df["Marks"] > 80)
-print(df["Marks"].mean())
-print(df.describe())
+# df = pd.DataFrame(data)
+# print(df.shape)
+# print(df.Marks)
+# print(df["Marks"] > 80)
+# print(df["Marks"].mean())
+# print(df.describe())
+
+# // learn the loc and iloc 
+# import pandas as pd
+
+# data = {
+#     "Name": ["Aman", "Riya", "Rahul", "Neha", "Vikas", "Priya"],
+#     "Age": [20, 21, 19, 22, 20, 21],
+#     "Marks": [78, 91, 65, 88, 72, 95],
+#     "City": ["Pune", "Mumbai", "Pune", "Delhi", "Mumbai", "Delhi"]
+# }
+
+# df = pd.DataFrame(data)
+# print(df[["Name","Marks"]])
+# print(df.columns)
+# print(df.loc[1])
+# print(df[(df["Marks"] > 80 )& (df["Age"]>20)])
+# print(df.sort_values("Marks"))
+# print(df.loc[df["City"]=="Delhi"])
+
+#small student analyser
+# import pandas as pd
+
+# data = {
+#     "Name": ["Aman", "Riya", "Rahul", "Neha", "Vikas", "Priya"],
+#     "Age": [20, 21, 19, 22, 20, 21],
+#     "Marks": [78, 91, 65, 88, 72, 95],
+#     "City": ["Pune", "Mumbai", "Pune", "Delhi", "Mumbai", "Delhi"]
+# }
+# df = pd.DataFrame(data)
+# print(df)
+# print(df["Marks"]>80)
+# print(df["Marks"].max())
+# print(df.sort_values("Marks",ascending = False)
+# def check_result(marks):
+#     if marks >= 40:
+#         return "Pass"
+#     else:
+#         return "Fail"
+
+# df["Result"] = df["Marks"].apply(check_result)
+
+# print(df)
